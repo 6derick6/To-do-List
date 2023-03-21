419fdd15b90f448dd4ecd509e7e12ade4d1a3996
6derick6 <6derick6@gmail.com>
2023-03-21 13:04:36 -0300

    App lista de tarefas 1/4

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
+
+  const image = require('./resources/bg.jpg');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,marginTop:30}}>
+        <ImageBackground source={image} style={styles.image}>
+          <View style={styles.coverView}>
+            <Text style={styles.textHeader}>To-do LIST</Text>
+          </View>
+        </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width:'100%',
+    height: 80,
+    resizeMode:'cover'
   },
+  
+  coverView: {
+    width:'100%',
+    height:80,
+    backgroundColor:'rgba(0,0,0,0.4)'
+  },
+
+  textHeader: {
+    textAlign:'center',
+    color:'white',
+    fontSize:22,
+    marginTop:20
+  }
 });
