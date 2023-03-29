d73b925b3e58ab644f18cad0f101c9aded9db0ba
6derick6 <6derick6@gmail.com>
2023-03-29 12:41:42 -0300

    App lista de tarefa extra #1

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 import { AppLoading } from 'expo';
+import { AntDesign } from '@expo/vector-icons'; 
+import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity,TouchableHighlight ,Modal ,ScrollView, TextInput } from 'react-native';
 
 export default function App() {
 
   const image = require('./resources/bg.jpg');
 
-  const [tarefas, setTarefas] = useState([
+  console.disableYellowBox = true;
+
+  const [tarefas, setarTarefas] = useState([
     {
-      id: 1,
-      tarefa: 'MInha primeira tarefa.'
+        id: 1,
+        tarefa: 'Minha tarefa 1.'
     },
     {
       id: 2,
-      tarefa: 'MInha segunda tarefa.'
+      tarefa: 'minha outra tarefa..'
+    },
+    {
+      id: 3,
+      tarefa: 'minha outra tarefa 3..'
     }
-  ]);
+]);
 
+  const [modal,setModal] = useState(true);
+
+
+ 
   let [fontsLoaded] = useFonts({
-    Inter_900Black,
+    Lato_400Regular,
   });
 
   if (!fontsLoaded) {
@@ -29,40 +39,75 @@ export default function App() {
   }
 
   function deletarTarefa(id){
-    let newTarefas = tarefas.filter(function(val){
-      return val.id != id;
-    });
+      alert('Tarefa com id '+id+' foi deletada com sucesso!');
+      //TODO: Deletar do array/estado a tarefa com id especificado!
+      let newTarefas = tarefas.filter(function(val){
+            return val.id != id;
+      });
 
-    setTarefas(newTarefas);
+      setarTarefas(newTarefas);
   }
 
   return (
-    <ScrollView style={{flex:1,marginTop:30}}>
+    
+    <ScrollView style={{flex:1}}>
       <StatusBar hidden />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput autoFocus={true}></TextInput>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModal(!modal);
+              }}
+            >
+              <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      
 
         <ImageBackground source={image} style={styles.image}>
           <View style={styles.coverView}>
-            <Text style={styles.textHeader}>To-do LIST</Text>
-          </View>
+            <Text style={styles.textHeader}>Lista de Tarefas - Danki Code</Text>
+            </View>
         </ImageBackground>
+
+      
 
         {
         tarefas.map(function(val){
-         return ( 
-          <View style={styles.tarefaSingle}>
-            <View style={{flex:1,width:'100%',padding:10}}> 
-              <Text>{val.tarefa}</Text>
+          return (<View style={styles.tarefaSingle}>
+            <View style={{flex:1,width:'100%',padding:10}}>
+                <Text>{val.tarefa}</Text>
             </View>
-
             <View style={{alignItems:'flex-end',flex:1,padding:10}}>
-              <TouchableOpacity onPress={()=> deletarTarefa(val.id)}><AntDesign name='minuscircleo' size={24} color='black' /></TouchableOpacity>
+              <TouchableOpacity onPress={()=> deletarTarefa(val.id)}><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
             </View>
-          </View>);
+            </View>);
         })
+        
 
         }
 
-    </ScrollView>
+        <TouchableOpacity style={styles.btnAddTarefa} onPress={()=>setModal(true)}><Text
+         style={{textAlign:'center',color:'white'}}>Adicionar Tarefa!
+         </Text>
+         </TouchableOpacity>
+        
+        </ScrollView>
+       
   );
 }
 
@@ -70,29 +115,71 @@ const styles = StyleSheet.create({
   image: {
     width:'100%',
     height: 80,
-    resizeMode:'cover'
+    resizeMode: "cover"
   },
-  
-  coverView: {
+  btnAddTarefa:{
+    width:200,
+    padding:8,
+    backgroundColor:'gray',
+    marginTop:20
+  },
+  coverView:{
     width:'100%',
     height:80,
-    backgroundColor:'rgba(0,0,0,0.4)'
+    backgroundColor:'rgba(0,0,0,0.5)'
   },
-
-  textHeader: {
+  textHeader:{
     textAlign:'center',
     color:'white',
-    fontSize:22,
-    marginTop:20,
-    fontFamily:'Inter_900Black'
-  },
-
-  tarefaSingle: {
+    fontSize:20,
     marginTop:30,
-    width:'100%',
-    borderBottomWidth:1,
-    borderBottomColor:'black',
-    flexDirection:'row',
-    paddingBottom:10
+    fontFamily:'Lato_400Regular'
+  },
+  tarefaSingle:{
+      marginTop:30,
+      width:'100%',
+      borderBottomWidth:1,
+      borderBottomColor:'black',
+      flexDirection:'row',
+      paddingBottom:10
+  },
+  //Estilos para nossa modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:'rgba(0,0,0,0.5)'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex:5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
+
 });
